a70c374c492b0a192a9808e3cbdc79fd18dc4f80
parth <parthbhutada13@gmail.com>
2025-12-16 00:37:48 +0530

    updated main.py
    Added necessary libraries for customer clustering analysis.

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+# #Importing Libraries
 
+
+#mall customers clustering analysis
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler, LabelEncoder, MinMaxScaler
+from sklearn.cluster import KMeans, DBSCAN, AgglomerativeClustering
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+from sklearn.metrics import silhouette_score, calinski_harabasz_score
+from scipy.cluster.hierarchy import dendrogram, linkage
